d98c50fe5ca9318a57ed655f07497ab9b4a3b2fc
_CODE__KRATOS_ <Abhishekroy2342005@gmail.com>
2025-03-15 02:22:14 +0530

    Added Footer to the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { useEffect } from "react";
 import Scheduler from "./components/Scheduler";
+import GitHubFooter from "./components/GitHubFooter";
 import "./App.css";
 
 function App() {
-  
-  useEffect(() => {
-    document.title = "CPU Scheduling Simulator";
-  }, []);
-
   return (
     <div className="bg-black">
         <div className="m-auto p-6 bg-black">
         <Scheduler />
         </div>
+        <GitHubFooter />
     </div>
   );
 }
