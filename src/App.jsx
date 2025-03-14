61816e721d65dbfcfe9e8b73941b9df67beb6b71
_CODE__KRATOS_ <Abhishekroy2342005@gmail.com>
2025-03-15 01:07:04 +0530

    Changed basic details

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import Scheduler from "./components/Scheduler";
 import "./App.css";
 
 function App() {
+  
+  useEffect(() => {
+    document.title = "CPU Scheduling Simulator";
+  }, []);
+
   return (
     <div className="bg-black">
         <div className="m-auto p-6 bg-black">
