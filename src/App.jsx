6ae3e3f00e936827ebfd410f2105a1a19fed79e7
_CODE__KRATOS_ <Abhishekroy2342005@gmail.com>
2025-03-15 10:24:51 +0530

    Resolved the deployment issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import GitHubFooter from "./components/GitHubFooter";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    document.title = "CPU Scheduler";
+  }, []);
   return (
     <div className="bg-black">
         <div className="m-auto p-6 bg-black">
