c56bec623c313a7d21272c97888f16cb316f94d3
dev <devkumar3631@gmail.com>
2025-06-25 20:49:52 +0530

    update build

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import Rect from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-//import FrontPage from "./pages/FrontPage";
 import Scheduler from "./components/Scheduler";
 import DiskSimulator from "./components/DiskSimulator";
 import FrontPage from "./components/Frontpage";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/operating_system_algorithm_simulator">
       <Routes>
         <Route path="/" element={<FrontPage />} />
         <Route path="/cpu-scheduling" element={<Scheduler />} /> 
