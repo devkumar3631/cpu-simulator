a249c2edd61d9ea29da63466ec64465e433024c6
dev <devkumar3631@gmail.com>
2025-06-26 08:39:54 +0530

    all good

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import Rect from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//import FrontPage from "./pages/FrontPage";
 import Scheduler from "./components/Scheduler";
 import DiskSimulator from "./components/DiskSimulator";
 import FrontPage from "./components/Frontpage";
 
 function App() {
   return (
+    <Router>
       <Routes>
         <Route path="/" element={<FrontPage />} />
         <Route path="/cpu-scheduling" element={<Scheduler />} /> 
         <Route path="/disk-simulator" element={<DiskSimulator />} /> 
       </Routes>
+    </Router>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: "/operating_system_algorithm_simulator/",
   plugins: [react(), tailwindcss()]
 });
